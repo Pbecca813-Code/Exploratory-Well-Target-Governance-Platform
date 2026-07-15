163b9cbd7b6ce2fb8a24cef2c0811328483d551d
Pbecca813-Code <pattybecca13881@gmail.com>
2026-07-15 16:53:06 -0300

    Implemented password reset and administrator access workflow

--- config/settings.py
+++ config/settings.py
@@ -147,3 +147,21 @@ PASSWORD_RESET_TIMEOUT = 3600
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
+
+# ===================================================
+# EMAIL CONFIGURATION
+# ===================================================
+
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.gmail.com"
+
+EMAIL_PORT = 587
+
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = "exploratory.welltarget@gmail.com"
+
+EMAIL_HOST_PASSWORD = "wgfd qqzp tbus lzeg"
+
+DEFAULT_FROM_EMAIL = "WellTarget Governance <exploratory.welltarget@gmail.com>"
